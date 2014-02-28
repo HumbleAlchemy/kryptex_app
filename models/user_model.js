a543942308bb31537465a9f49179e5b5131237d0
Surbhi Shah <surbhishah81@yahoo.in>
2014-02-28 15:24:35 +0545

    user data related operations including adduser, check existance

--- models/user_model.js
+++ models/user_model.js
@@ -1,10 +1,52 @@
+/*
+	user_email
 
-module.exports.checkUser = function(username,password,callback) {
-
-	if(username == 'abhi' || username == 'kuku') {
-		return callback(null,1);
-	} else {
-		return callback("Invalid username password combi",null);
-	}
+	user_password
 	
-}
+	user_name
+	
+	user_ph_no
+
+*/
+
+/*  keys for redis hashset */
+
+var userSchema = {
+	name : "name",
+	password : "password",
+	ph_no : "ph_no",
+	email : "email",
+	current_level : "current_level",
+	wildcard_count : "wildcard_count"
+};
+
+
+/* MOdule for adding user details */
+
+exports.adduser = function(db, user_email, user_password,user_name, user_ph_no){
+	if( check_for_user(db, user_name) == 0){
+		db.hset("user:"+user_name,
+			userSchema.email, user_email, 
+			userSchema.password, user_password,
+		  	userSchema.name, user_name,
+		   	userSchema.ph_no, user_ph_no,
+		    userSchema.current_level, 0,
+		    userSchema.wildcard_count, 2,
+			function (err, status){
+				if(err) console.log("ËRR IN FETCHING DB AT adduser");
+			}
+		);	
+	}
+};
+
+/* MOdule for checking existance of user */
+
+exports.check_for_id = function(db, user_name){
+	db.exists("user:"+ user_name, function (err, status){
+		if(err) console.log("unable to fetch data at check_for_id");
+		return status;
+	});
+};
+
+
+
