1524a87a0f9ac4d0ee572f70b9065aa0eafb3445
Abhishek Nair <abhishek.alchemist@gmail.com>
2014-02-28 16:52:08 +0530

    changes to score user model with score functions

---
+++ models/level_model.js
@@ -0,0 +1,23 @@
+
+var LevelSchema = {
+	schema_name = "level",
+	image_path = 0,
+	solution = 1
+}
+
+
+modeule.exports.add_level = function() {
+
+}
+
+module.exports.get_level_image = function(level_no,db) {
+	key = LevelSchema.schema_name + ':' + level_no;
+	level_details = db.lrange(key,function(err,data) {
+		if(!err) {
+			return level_details[LevelSchema.image_path];
+		} else {
+			console.log("ERROR: level_model.js , get_level module");
+			return null;
+		}
+	});
+}
--- models/user_model.js
+++ models/user_model.js
@@ -20,9 +20,12 @@ var userSchema = {
 	wildcard_count : "wildcard_count"
 };
 
+var scoreSchema = {
+	set_name : "scores"
+};
+
 
 /* MOdule for adding user details */
-
 exports.adduser = function(db, user_email, user_password,user_name, user_ph_no){
 	if( check_for_user(db, user_name) == 0){
 		db.hset("user:"+user_name,
@@ -33,14 +36,13 @@ exports.adduser = function(db, user_email, user_password,user_name, user_ph_no){
 		    userSchema.current_level, 0,
 		    userSchema.wildcard_count, 2,
 			function (err, status){
-				if(err) console.log("ËRR IN FETCHING DB AT adduser");
+				if(err) console.log("ERR IN FETCHING DB AT adduser");
 			}
 		);	
 	}
 };
 
 /* MOdule for checking existance of user */
-
 exports.check_for_id = function(db, user_name){
 	db.exists("user:"+ user_name, function (err, status){
 		if(err) console.log("unable to fetch data at check_for_id");
@@ -48,5 +50,17 @@ exports.check_for_id = function(db, user_name){
 	});
 };
 
+/*change user score */
+exports.increment_user_score = function(user_name) {
+	db.zincr(scoreSchema.set_name,1,user_name,function(err,status) {
+		if(!err) {
+			return 1;
+		} else {
+			return 0;
+		}
+	});
+}
 
-
+exports.get_user_score = function(user_name) {
+	db.z
+}
